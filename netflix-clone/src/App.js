7b6e526936286705c627c7a1bea562eb0b8fc7a2
SanjayAmirneni <sanjayamirneni@gmail.com>
2022-03-08 21:44:18 -0600

    Second Commit

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -12,12 +12,12 @@ function App() {
       <Banner request={requests.fetchNetflixOriginals}/>
       <Row title="NETFLIX ORIGINALS" request={requests.fetchNetflixOriginals} large="true" />
       <Row title="Trending Now" request={requests.fetchTrending} />
-      {/* <Row title="Top Rated" request={requests.fetchTopRated}/>
+      <Row title="Top Rated" request={requests.fetchTopRated}/>
       <Row title="Action Movies" request={requests.fetchActionMovies}/>
       <Row title="Comedy Movies" request={requests.fetchComedyMovies}/>
       <Row title="Horror Movies" request={requests.fetchHorrorMovies}/>
       <Row title="Romance Movies" request={requests.fetchRomanceMovies}/>
-      <Row title="Documentaries" request={requests.fetchDocumantaries}/> */}
+      <Row title="Documentaries" request={requests.fetchDocumantaries}/>
     </div>
   );
 }
